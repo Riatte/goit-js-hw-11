d59fb768aa937555a8330882580d47cb84953c0a
Varvara Babych <varvara.babich13@gmail.com>
2023-02-04 20:35:01 +0200

    changes 03-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+
+const form = document.querySelector('.form');
+const formElem = form.elements;
+
+form.addEventListener('submit', promiseSubmit);
+
+
+
+
+function promiseSubmit(evt) {
+  evt.preventDefault();
+
+  const amount = +formElem.amount.value;
+const step = +formElem.step.value;
+let delay = +formElem.delay.value;
+
+  for (let i = 1; i <= amount; i++) {
+    if (i > 1) {
+      delay += step;
+    }
+  createPromise(i, delay)
+  .then(({ position, delay }) => {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
   }
+}
+
+
+function createPromise(position, delay) {
+   const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {  
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
